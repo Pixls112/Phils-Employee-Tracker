a5ca4a5cfd0f4817b8b0feeb6609535dc5e260a3
Pixls112 <nguyen.phillip112@gmai.com>
2023-09-05 19:19:13 -0700

    added some data to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department(name)
+VALUES ("Audio"), 
+("Programmer"), 
+("Art/Design"), 
+("Tester");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Game Audio Designer", 150000, 1), 
+("Game Programmer", 113000, 2),
+("Game Art Designer", 112000, 3), 
+("Game Teser", 40000, 4)
