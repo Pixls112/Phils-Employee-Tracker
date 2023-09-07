d8d8811880e5be27192d2e9d4a4d24ef55b3412f
Pixls112 <nguyen.phillip112@gmai.com>
2023-09-06 18:47:03 -0700

    added references for each data set in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -12,6 +12,7 @@ CREATE TABLE roles(
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -20,4 +21,6 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 )
